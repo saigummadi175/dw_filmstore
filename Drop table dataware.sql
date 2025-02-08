72c54a7c0ce145e65954639274754158d52044ba
indumyneni <neni34grand@gmail.com>
2025-02-08 18:05:59 -0500

    Add files via upload

---
+++ Drop table dataware.sql
@@ -0,0 +1,11 @@
+
+
+
+DROP TABLE `dw_filmstore`.`d_actor`;
+DROP TABLE `dw_filmstore`.`d_customer`;
+DROP TABLE `dw_filmstore`.`d_film`;
+DROP TABLE `dw_filmstore`.`d_filmactor`;
+DROP TABLE `dw_filmstore`.`d_store_staff`;
+DROP TABLE `dw_filmstore`.`f_payment`;
+DROP TABLE `dw_filmstore`.`f_rental`;
+DROP TABLE `dw_filmstore`.`f_sales`;
