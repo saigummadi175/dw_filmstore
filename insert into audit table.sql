4de413755f8520f72cb5df20c321e034d343d608
saigummadi175 <chinni.1392@gmail.com>
2025-03-20 10:26:26 -0400

    Add files via upload

---
+++ insert into audit table.sql
@@ -0,0 +1,21 @@
+insert into sakila.audit_table (tablename, lastloaded_timestamp, currenttimestamp)
+values ('actor' ,'2025/01/01', current_timestamp());
+
+
+insert into sakila.audit_table 
+values ('address', '2025/01/01', current_timestamp()),
+        ('category', '2025/01/01', current_timestamp()),
+        ('city', '2025/01/01', current_timestamp()),
+        ('country', '2025/01/01', current_timestamp()),
+        ('customer', '2025/01/01', current_timestamp()),
+        ('film', '2025/01/01', current_timestamp()),
+        ('film_actor', '2025/01/01', current_timestamp()),
+        ('film_category', '2025/01/01', current_timestamp()),
+        ('city', '2025/01/01', current_timestamp()),
+        ('film_text', '2025/01/01', current_timestamp()),
+        ('inventory', '2025/01/01', current_timestamp()),
+        ('language', '2025/01/01', current_timestamp()),
+        ('payment', '2025/01/01', current_timestamp()),
+        ('rental', '2025/01/01', current_timestamp()),
+        ('staff', '2025/01/01', current_timestamp()),
+        ('store', '2025/01/01', current_timestamp());
