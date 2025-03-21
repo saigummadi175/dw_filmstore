711280529e3642cff9651dd4792e777304f50860
saigummadi175 <chinni.1392@gmail.com>
2025-03-21 10:57:09 -0400

    Add files via upload

---
+++ apply_staging.sql
@@ -0,0 +1,369 @@
+
+/*actor*/
+
+begin;
+
+update stg_sakila.stg_actor sa
+join stg_sakila.int_actor ia on sa.actor_id= ia.actor_id
+set sa.first_name = ia.first_name,
+sa.last_name = ia.last_name,
+sa.last_update = current_timestamp();
+;	
+
+insert into stg_sakila.stg_actor 
+select * from stg_sakila.int_actor ia
+where not exists (select actor_id
+                   from stg_sakila.stg_actor  sa
+                   where ia.actor_id=sa.actor_id);
+
+
+delete from stg_sakila.int_actor;
+
+commit;
+
+/*address*/
+
+
+begin;
+
+update stg_sakila.stg_address sa
+join stg_sakila.int_address ia on sa.address_id= ia.address_id
+set sa.address = ia.address,
+sa.address2 = ia.address2,
+sa.district = ia.district,
+sa.city_id = ia.city_id,
+sa.postal_code = ia.postal_code,
+sa.phone = ia.phone,
+sa.location = ia.location,
+sa.last_update = current_timestamp();
+;	
+
+insert into stg_sakila.stg_address 
+select * from stg_sakila.int_address ia
+where not exists (select 1
+                   from stg_sakila.stg_address  sa
+                   where ia.address_id=sa.address_id);
+
+
+delete from stg_sakila.int_address;
+
+commit;
+
+
+/*category*/
+
+
+begin;
+
+update stg_sakila.stg_category sc
+join stg_sakila.int_category ic on sc.category_id= ic.category_id
+set sc.name = ic.name,
+sc.last_update = current_timestamp();
+;	
+
+insert into stg_sakila.stg_category
+select * from stg_sakila.int_category ic
+where not exists (select 1
+                   from stg_sakila.stg_category  sc
+                   where ic.category_id=sc.category_id);
+
+
+delete from stg_sakila.int_category;
+
+commit;
+
+/* city */
+
+
+begin;
+
+update stg_sakila.stg_city sct
+join stg_sakila.int_city ict on sct.city_id= ict.city_id
+set 
+sct.city = ict.city,
+sct.country_id = ict.country_id
+;	
+
+insert into stg_sakila.stg_city
+select * from stg_sakila.int_city ict
+where not exists (select 1
+                   from stg_sakila.stg_city  sct
+                   where ict.city_id=sct.city_id);
+
+
+delete from stg_sakila.int_city;
+
+commit;
+
+/* country */
+
+
+begin;
+
+update stg_sakila.stg_country scy
+join stg_sakila.int_country icy on scy.country_id= icy.country_id
+set 
+scy.country = icy.country
+;	
+
+insert into stg_sakila.stg_country
+select * from stg_sakila.int_country icy
+where not exists (select 1
+                   from stg_sakila.stg_country  scy
+                   where icy.country_id=scy.country_id);
+
+
+delete from stg_sakila.int_country;
+commit;
+
+
+/* customer */
+
+begin;
+
+update stg_sakila.stg_customer scm
+join stg_sakila.int_customer icm on scm.customer_id= icm.customer_id
+set 
+scm.store_id = icm.store_id,
+scm.first_name = icm.first_name,
+scm.last_name = icm.last_name,
+scm.email = icm.email,
+scm.address_id = icm.address_id,
+scm.active = icm.active
+;	
+
+insert into stg_sakila.stg_customer
+select * from stg_sakila.int_customer icm
+where not exists (select 1
+                   from stg_sakila.stg_customer scm
+                   where icm.customer_id=scm.customer_id);
+
+
+delete from stg_sakila.int_customer;
+commit;
+
+/* film */
+
+begin;
+
+update stg_sakila.stg_film sf
+join stg_sakila.int_film ifl on sf.film_id= ifl.film_id
+set 
+sf.rental_duration = ifl.rental_duration,
+sf.rental_rate = ifl.rental_date,
+sf.length = ifl.length,
+sf.replacement_cost = ifl.replacement_cost,
+sf.rating = ifl.rating
+
+;	
+
+insert into stg_sakila.stg_film
+select * from stg_sakila.int_film ifl
+where not exists (select 1
+                   from stg_sakila.stg_film sf
+                   where sf.film_id= ifl.film_id);
+
+
+delete from stg_sakila.int_film;
+commit;
+
+
+/* film_actor */ 
+
+
+begin;
+
+update stg_sakila.stg_film_actor sfa
+join stg_sakila.int_film_actor ifa on sfa.actor_id= ifa.actor_id
+set 
+sfa.film_id = ifa.film_id
+
+;	
+
+insert into stg_sakila.stg_film_actor
+select * from stg_sakila.int_film_actor ifa
+where not exists (select 1
+                   from stg_sakila.stg_film_actor sfa
+                   where sfa.actor_id= ifa.actor_id);
+
+
+delete from stg_sakila.int_film_actor;
+commit;
+
+
+/* film_category*/
+
+
+begin;
+
+update stg_sakila.stg_film_category sfc
+join stg_sakila.int_film_category ifc on sfc.category_id= ifc.category_id
+set 
+sfc.film_id = ifc.film_id
+
+;	
+
+insert into stg_sakila.stg_film__category
+select * from stg_sakila.int_film__category ifc
+where not exists (select 1
+                   from stg_sakila.stg_film__category sfc
+                   where sfc.category_id= ifc.category_id);
+
+
+delete from stg_sakila.int_film__category;
+commit;
+
+/* inventory */
+
+begin;
+
+update stg_sakila.stg_inventory si
+join stg_sakila.int_inventory ii on si.inventory_id= ii.inventory_id
+set 
+si.film_id = ii.film_id,
+si.store_id = ii.store_id
+
+;	
+
+insert into stg_sakila.stg_inventory
+select * from stg_sakila.int_inventory ii
+where not exists (select 1
+                   from stg_sakila.stg_inventory si
+                   where si.inventory_id= ii.inventory_id);
+
+
+delete from stg_sakila.int_inventory;
+commit;
+
+
+/* language*/
+
+
+begin;
+
+update stg_sakila.stg_language sl
+join stg_sakila.int_language il on sl.language_id= il.language_id
+set 
+sl.name = il.name
+
+;	
+
+insert into stg_sakila.stg_language
+select * from stg_sakila.int_language il
+where not exists (select 1
+                   from stg_sakila.stg_language sl
+                   where sl.language_id= il.language_id);
+
+
+delete from stg_sakila.int_language;
+commit;
+
+
+
+/* payment */
+
+
+begin;
+
+update stg_sakila.stg_payment sp
+join stg_sakila.int_payment ip on sp.payment_id= ip.payment_id
+set 
+sp.staff_id = ip.staff_id,
+sp.rental_id = ip.rental_id,
+sp.amount = ip.amount,
+sp.payment_date = ip.payment_date
+
+;	
+
+insert into stg_sakila.stg_payment
+select * from stg_sakila.int_payment ip
+where not exists (select 1
+                   from stg_sakila.stg_payment sp
+                   where sp.payment_id= ip.payment_id);
+
+
+delete from stg_sakila.int_payment;
+commit;
+
+
+
+/* rental */
+
+
+
+begin;
+
+update stg_sakila.stg_rental sr
+join stg_sakila.int_rental ir on sr.rental_id= ir.rental_id
+set 
+sr.staff_id = ir.staff_id,
+sr.rental_date = ir.rental_date,
+sr.return_date = ir.return_date
+
+;	
+
+insert into stg_sakila.stg_rental
+select * from stg_sakila.int_rental ir
+where not exists (select 1
+                   from stg_sakila.stg_rental sr
+                   where sr.rental_id= ir.rental_id);
+
+
+delete from stg_sakila.int_rental;
+commit;
+
+
+/* staff */
+
+
+begin;
+
+update stg_sakila.stg_staff ssf
+join stg_sakila.int_staff isf on ssf.staff_id= isf.staff_id
+set 
+ssf.last_name = isf.last_name,
+ssf.address_id = isf.address_id,
+ssf.picture = isf.picture,
+ssf.email = isf.email,
+ssf.store_id = isf.store_id,
+ssf.active = isf.active,
+ssf.username = isf.username,
+ssf.password = isf.password
+
+;	
+
+insert into stg_sakila.stg_staff
+select * from stg_sakila.int_staff isf
+where not exists (select 1
+                   from stg_sakila.stg_staff ssf
+                   where ssf.staff_id= isf.staff_id);
+
+
+delete from stg_sakila.int_staff;
+commit;
+
+
+/* store */
+
+
+begin;
+
+update stg_sakila.stg_store sst
+join stg_sakila.int_store ist on sst.store_id= ist.store_id
+set 
+sst.manager_staff_id = ist.manager_staff_id,
+sst.address_id = ist.address_id
+;	
+
+insert into stg_sakila.stg_store
+select * from stg_sakila.int_store ist
+where not exists (select 1
+                   from stg_sakila.stg_store sst
+                   where sst.store_id= ist.store_id);
+
+
+delete from stg_sakila.int_store;
+commit;
+
+
+
